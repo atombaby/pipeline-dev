ca7c88dc8d5e49d89ceb992ba14216762ab4a201
Amy Paguirigan <amypaguirigan@gmail.com>
2016-09-22 10:40:15 -0700

    Added email for failures

--- batchScriptMaker-TargDNASeq.sh
+++ batchScriptMaker-TargDNASeq.sh
@@ -13,6 +13,8 @@ for s in ${samples}; do
     echo "#SBATCH -N1" >> ${sbatch}
     echo "#SBATCH -n4" >> ${sbatch}
     echo "#SBATCH -t0-12" >> ${sbatch}
+    echo "#SBATCH --mail-type=FAIL" >> ${sbatch}
+    echo "#SBATCH --mail-user=apaguiri@fredhutch.org" >> ${sbatch}
     echo "#SBATCH -o '${base}-%j.out'" >> ${sbatch}
     echo "echo 'Processing' ${s}" >> ${sbatch}
     echo "/fh/fast/paguirigan_a/GenomicsArchive/TruSightMyeloid/pipe-TruSight-TargDNA-UnPaired-Rclean.sh ${s}" >> ${sbatch}
