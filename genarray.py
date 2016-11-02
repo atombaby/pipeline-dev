4e43b72aa81f17a055b0059b5e448dd84aaacc71
Michael Gutteridge <michael.gutteridge@gmail.com>
2016-11-02 13:11:19 -0700

    refactored genarray

--- genarray.py
+++ genarray.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
 
-list = [1,3,5,6,7,8,12,14,15,16,22]
-job_array = []
-i = 0
-while i < len(list):
-    print(job_array)
-    c = list[i]
-    n = list[i+1]
-    print("DEBUG: at {} current is {}, next is {}".format(i,c,n))
-    if n == c + 1:
-        seq_start = c
-        print("DEBUG: found sequence starting at {}".format(seq_start))
-        while n == c + 1:
-            i+=1
-            c = list[i]
-            n = list[i+1]
-            print("DEBUG: at {} current is {}, next is {}".format(i,c,n))
-        seq_end = c
-        i += 1
-        print("{}-{}".format(seq_start,seq_end))
-        job_array.append("{}-{}".format(seq_start,seq_end))
-    else:
-        print(c)
-        job_array.append("{}".format(c))
-        i += 1
+import sys
+from itertools import groupby
+from operator import itemgetter
+
+def group_index( l ):
+    # for list of ints in `l`, return list of grouped indexes
+    out = []
+    for k, g in groupby(enumerate(l), lambda x : x[1] - x[0]):
+        out.append(list(map(itemgetter(1), g)))
+    return out
+
+def format_for_sbatch( l ):
+    # take a list of grouped numbers and format for `sbatch -a <>`
+    # [[1],[3,4,5,6],[9,10],[12]]
+    out = []
+    print(l)
+    for g in l:
+        print(g)
+        if g[0] == g[-1]:
+            out.append(str(g[0]))
+        else:
+            out.append("{}-{}".format( g[0], g[-1] ))
+
+    return ','.join(out)
+
+if __name__ == "__main__":
+    numbers = sys.argv[1].split(',')
+    try:
+        numbers = [ int(n) for n in numbers ]
+    except ValueError:
+        print("ERROR: non-integer found in your list of numbers")
+        sys.exit(1)
+
+    numbers = sorted( numbers )
+    a = group_index( numbers )
+    print(format_for_sbatch( a ))
 
